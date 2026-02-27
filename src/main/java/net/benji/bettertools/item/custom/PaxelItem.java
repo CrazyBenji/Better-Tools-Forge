9485abcaab5f93ad079404a2a1776293b2bdf30c
CrazyBenji <benlands101@gmail.com>
2026-02-27 14:48:27 -0500

    Made Paxel logic more compatible with other mods

--- src/main/java/net/benji/bettertools/item/custom/PaxelItem.java
+++ src/main/java/net/benji/bettertools/item/custom/PaxelItem.java
@@ -19,6 +19,8 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraftforge.common.ToolAction;
+import net.minecraftforge.common.ToolActions;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -45,80 +47,67 @@ public class PaxelItem extends DiggerItem {
         BlockState blockState = level.getBlockState(blockPos);
 
         // Axe Logic
-        Optional<BlockState> optional = this.getStripped(blockState);
-        Optional<BlockState> optional2 = WeatheringCopper.getPrevious(blockState);
-        Optional<BlockState> optional3 = Optional.ofNullable((Block)((BiMap<?, ?>)HoneycombItem.WAX_OFF_BY_BLOCK.get()).get(blockState.getBlock()))
-                .map(block -> block.withPropertiesOf(blockState));
-        ItemStack itemStack = context.getItemInHand();
-        Optional<BlockState> optional4 = Optional.empty();
+        Optional<BlockState> optional = Optional.ofNullable(blockState.getToolModifiedState(context, ToolActions.AXE_STRIP, false));
+        Optional<BlockState> optional1 = optional.isPresent() ? Optional.empty() : Optional.ofNullable(blockState.getToolModifiedState(context, ToolActions.AXE_SCRAPE, false));
+        Optional<BlockState> optional2 = optional.isEmpty() && optional1.isEmpty() ? Optional.ofNullable(blockState.getToolModifiedState(context, ToolActions.AXE_WAX_OFF, false)) : Optional.empty();
+        ItemStack itemstack = context.getItemInHand();
+        Optional<BlockState> optional3 = Optional.empty();
         if (optional.isPresent()) {
             level.playSound(player, blockPos, SoundEvents.AXE_STRIP, SoundSource.BLOCKS, 1.0F, 1.0F);
-            optional4 = optional;
-        } else if (optional2.isPresent()) {
+            optional3 = optional;
+        } else if (optional1.isPresent()) {
             level.playSound(player, blockPos, SoundEvents.AXE_SCRAPE, SoundSource.BLOCKS, 1.0F, 1.0F);
             level.levelEvent(player, 3005, blockPos, 0);
-            optional4 = optional2;
-        } else if (optional3.isPresent()) {
+            optional3 = optional1;
+        } else if (optional2.isPresent()) {
             level.playSound(player, blockPos, SoundEvents.AXE_WAX_OFF, SoundSource.BLOCKS, 1.0F, 1.0F);
             level.levelEvent(player, 3004, blockPos, 0);
-            optional4 = optional3;
+            optional3 = optional2;
         }
 
-        if (optional4.isPresent()) {
+        if (optional3.isPresent()) {
             if (player instanceof ServerPlayer) {
-                CriteriaTriggers.ITEM_USED_ON_BLOCK.trigger((ServerPlayer)player, blockPos, itemStack);
+                CriteriaTriggers.ITEM_USED_ON_BLOCK.trigger((ServerPlayer)player, blockPos, itemstack);
             }
 
-            level.setBlock(blockPos, optional4.get(), 11);
-            level.gameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Context.of(player, optional4.get()));
+            level.setBlock(blockPos, optional3.get(), 11);
+            level.gameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Context.of(player, optional3.get()));
             if (player != null) {
-                itemStack.hurtAndBreak(1, player, (player1) -> player1.broadcastBreakEvent(context.getHand()));
+                itemstack.hurtAndBreak(1, player, (p_150686_) -> p_150686_.broadcastBreakEvent(context.getHand()));
             }
 
             return InteractionResult.sidedSuccess(level.isClientSide);
         }
 
         // Shovel Logic
-        if (context.getClickedFace() == Direction.DOWN) {
-            return InteractionResult.PASS;
-        } else {
-            BlockState blockState2 = getFlattened(blockState);
-            BlockState blockState3 = null;
-            if (blockState2 != null && level.getBlockState(blockPos.above()).isAir()) {
+        if (context.getClickedFace() != Direction.DOWN) {
+            BlockState blockstate1 = blockState.getToolModifiedState(context, ToolActions.SHOVEL_FLATTEN, false);
+            BlockState blockstate2 = null;
+            if (blockstate1 != null && level.isEmptyBlock(blockPos.above())) {
                 level.playSound(player, blockPos, SoundEvents.SHOVEL_FLATTEN, SoundSource.BLOCKS, 1.0F, 1.0F);
-                blockState3 = blockState2;
+                blockstate2 = blockstate1;
             } else if (blockState.getBlock() instanceof CampfireBlock && blockState.getValue(CampfireBlock.LIT)) {
                 if (!level.isClientSide()) {
-                    level.levelEvent(null, 1009, blockPos, 0);
+                    level.levelEvent((Player)null, 1009, blockPos, 0);
                 }
 
                 CampfireBlock.dowse(context.getPlayer(), level, blockPos, blockState);
-                blockState3 = blockState.setValue(CampfireBlock.LIT, Boolean.FALSE);
+                blockstate2 = blockState.setValue(CampfireBlock.LIT, false);
             }
 
-            if (blockState3 != null) {
+            if (blockstate2 != null) {
                 if (!level.isClientSide) {
-                    level.setBlock(blockPos, blockState3, 11);
-                    level.gameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Context.of(player, blockState3));
+                    level.setBlock(blockPos, blockstate2, 11);
+                    level.gameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Context.of(player, blockstate2));
                     if (player != null) {
-                        itemStack.hurtAndBreak(1, player, (player1) -> player1.broadcastBreakEvent(context.getHand()));
+                        context.getItemInHand().hurtAndBreak(1, player, (p_43122_) -> p_43122_.broadcastBreakEvent(context.getHand()));
                     }
                 }
 
                 return InteractionResult.sidedSuccess(level.isClientSide);
-            } else {
-                return InteractionResult.PASS;
             }
         }
-
-    }
-
-    private Optional<BlockState> getStripped(BlockState unstrippedState) {
-        return Optional.ofNullable(AxeItem.getAxeStrippingState(unstrippedState));
-    }
-
-    private BlockState getFlattened(BlockState unflattenedState) {
-        return ShovelItem.getShovelPathingState(unflattenedState);
+        return InteractionResult.PASS;
     }
 
     @Override
@@ -129,5 +118,12 @@ public class PaxelItem extends DiggerItem {
             tooltipComponents.add(CommonComponents.EMPTY);
             tooltipComponents.add(DESC);
         }
+    }
+
+    @Override
+    public boolean canPerformAction(ItemStack stack, ToolAction toolAction) {
+        return ToolActions.DEFAULT_AXE_ACTIONS.contains(toolAction)
+                || ToolActions.DEFAULT_SHOVEL_ACTIONS.contains(toolAction)
+                || ToolActions.DEFAULT_PICKAXE_ACTIONS.contains(toolAction);
     }
 }
